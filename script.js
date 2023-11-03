9ed56905a0de30aab90aa2265842cb6500001e01
GabiLenz <135671513+GabiLenz@users.noreply.github.com>
2023-11-02 21:54:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const tasks = [];
+
+function newId() {
+    // Gerar IDs entre 0 e 999
+    return Math.floor(Math.random() * 1000); 
+}
+
+function createTask(taskTitle, taskDescription = "") {
+    let id = newId();
+    
+    let task = {
+        id,
+        taskTitle: taskTitle,
+        taskDescription: taskDescription,
+    }
+
+    tasks.push(task);
+    // renderTasks();
+    return task;
+}
